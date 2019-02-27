e8d06a783ab3193b1a6c8fc3495bd9a18b2e3599
Lisu <lisembart97@gmail.com>
2019-02-27 21:57:37 +0100

    Created first prototype of serialization

--- Editor/TimeCounter.cs
+++ Editor/TimeCounter.cs
@@ -3,15 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using System.Xml.Serialization;
 
 [InitializeOnLoad]
 public class TimeCounter
 {
     private static DateTime startTime;
+    private static List<TimeSpan> sessions = new List<TimeSpan>();
+    private static string saveFilePath = @"D:\sessions.xml";
+
     static TimeCounter()
     {
         startTime = DateTime.Now;
         UnityEngine.Debug.Log("Current time " + startTime);
+
+        if (File.Exists(saveFilePath))
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<TimeSpan>));
+            using (StreamReader reader = new StreamReader(File.OpenRead(saveFilePath)))
+            {
+                sessions = (List<TimeSpan>) serializer.Deserialize(reader);
+            }
+        }
+
+        foreach (TimeSpan session in sessions)
+        {
+            Debug.Log("Session: " + session);
+        }
 
         EditorApplication.update += Update;
         EditorApplication.wantsToQuit += OnQuit;
@@ -19,15 +38,36 @@ public class TimeCounter
 
     public static void Update()
     {
-        Debug.Log("Apdejtuje");
+        
     }
 
     public static bool OnQuit()
     {
         DateTime nowTime = DateTime.Now;
         TimeSpan sessionTime = (nowTime - startTime);
-        Debug.Log("Unity quiting: " + sessionTime);
-        return true;
+        sessions.Add(sessionTime);
+        Debug.Log("Unity quiting, session time: " + sessionTime);
+        TimeSpan projectTIme;
+        foreach (TimeSpan session in sessions)
+        {
+            projectTIme += session;
+        }
+        Debug.Log("Total time: " + projectTIme);
+
+        bool serialized = false;
+        XmlSerializer serializer = new XmlSerializer(typeof(List<TimeSpan>));
+        using (FileStream fileStream = new FileStream(saveFilePath, FileMode.Create))
+        {
+            serializer.Serialize(fileStream, sessions);
+            serialized = true;
+        }
+
+        foreach (TimeSpan session in sessions)
+        {
+            Debug.Log("Sessions at end: " + session);
+        }
+
+        return false;
     }
 
 
