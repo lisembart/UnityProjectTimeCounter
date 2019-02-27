15256a06a463358ab1ea025abb8a5abac3d2b304
= <lisembart97@gmail.com>
2019-02-27 21:15:42 +0100

    Added first prototype of time counting

---
+++ Editor/TimeCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class TimeCounter
+{
+    private static DateTime startTime;
+    static TimeCounter()
+    {
+        startTime = DateTime.Now;
+        UnityEngine.Debug.Log("Current time " + startTime);
+
+        EditorApplication.update += Update;
+        EditorApplication.wantsToQuit += OnQuit;
+    }
+
+    public static void Update()
+    {
+        Debug.Log("Apdejtuje");
+    }
+
+    public static bool OnQuit()
+    {
+        DateTime nowTime = DateTime.Now;
+        TimeSpan sessionTime = (nowTime - startTime);
+        Debug.Log("Unity quiting: " + sessionTime);
+        return true;
+    }
+
+
+}
